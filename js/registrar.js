4d5dc37e5f880fe845a83e6f70e8650c318ed75c
Sebastián Cárdenas <sebca87@gmail.com>
2021-09-27 22:01:37 -0500

    Se agrega la funcionalidad de recibir varias compras y mostrarlas en una tabla dinámica y se agrega la explicación del concepto de funciones en js

--- js/registrar.js
+++ js/registrar.js
@@ -34,18 +34,159 @@
                 // listaProductos.splice(1, 1);
                 
                 // console.log(listaProductos);
-                
+     
 
+                
+                function saludar(nombre, edad){
+                    let estado;
+                     if (edad>=18) {
+                         estado="Mayor de edad";
+                         
+                     } else {
+                         estado="Menor de edad";
+                     }
+                    return `Hola ${nombre} desde función, ${estado}`;
+                }
+
+                const saludar_flecha = (nombre, edad)=>{
+                    let estado;
+                     if (edad>=18) {
+                         estado="Mayor de edad";
+                         
+                     } else {
+                         estado="Menor de edad";
+                     }
+                    return `Hola ${nombre} desde función, ${estado}`;
+                };
+
+                const saludar_abreviado = (nombre) => `Hola ${nombre} desde función`;
+                
+                let mensaje = saludar_abreviado("Juan", 25);
+                document.write(mensaje);
+
+
+                
+                // listaProductos.forEach((elemento)=>{
+                    //     document.write(elemento);
+                    // });
+                    
+/*
 let listaProductos = ["Disco Duro", "Teclado", "Mouse", "Pantalla", "Memoria USB"];
 let valoresProductos = [200000, 100000, 80000, 800000, 50000];
                 
-let cliente = prompt("Escriba el nombre del cliente...");
-let documento = prompt(`Escriba el documento de ${cliente}...`);
+let producto1 = {"nombre":"Disco Duro", "precio":200000};
+let producto2 = {"nombre":"Teclado", "precio":100000};
+let producto3 = {"nombre":"Mouse", "precio":80000};
+let producto4 = {"nombre":"Pantalla", "precio":800000};
+let producto5 = {"nombre":"Memoria USB", "precio":50000};
+
+listaProductos = [producto1, producto2, producto3, producto4, producto5];
+
+let continuar;
 
 let menu = "";
 
 for (let i in listaProductos) {
-    menu += `${parseInt(i)+1}. ${listaProductos[i]} ($${valoresProductos[i]})\n`;
+    let producto = listaProductos[i];
+    menu += `${parseInt(i)+1}. ${producto.nombre} ($${producto.precio})\n`;
+}
+let nombresEnvio = ["Express", "Normal"];
+let valoresEnvio = [20000, 10000];
+
+let listadoCompras=[];
+
+do{
+    let compra={};
+
+    compra.cliente = prompt("Escriba el nombre del cliente...");
+    compra.documento = prompt(`Escriba el documento de ${compra.cliente}...`);
+
+    let seleccion = parseInt(prompt(`${menu}\nSeleccione el producto...`));
+
+    compra.producto = listaProductos[seleccion-1];
+
+    compra.cantidad = parseInt(prompt("Escriba la cantidad que desea llevar..."));
+    compra.envio = parseInt(prompt("Seleccione el tipo de envío (1. Express/2. Normal)..."));
+
+    compra.total=0;
+    compra.descuento=0;
+
+    let subtotal = compra.producto.precio*compra.cantidad;
+    let valorEnvio=0;
+
+    if(compra.cantidad>10){
+        compra.descuento = subtotal*0.1;
+    }
+
+
+    if(subtotal>5000000){
+        if (compra.envio==1) {
+            valorEnvio = valoresEnvio[compra.envio];
+            
+        } else {
+            valorEnvio=0;
+        }
+        compra.descuento += valoresEnvio[compra.envio-1] - valorEnvio;
+    }else{
+        valorEnvio = valoresEnvio[compra.envio-1];
+    }
+
+
+    compra.total = valorEnvio + subtotal - compra.descuento;
+
+    listadoCompras.push(compra);
+
+    // console.log(listadoCompras);
+
+    continuar = confirm("Ingresar otra compra?");
+
+}while(continuar);
+
+
+let xhtml = `<table>
+                <thead>
+                    <tr>
+                        <th>CLIENTE</th>
+                        <th>PRODUCTO</th>
+                        <th>CANTIDAD</th>
+                        <th>ENVÍO</th>
+                        <th>DESCUENTO</th>
+                        <th>TOTAL</th>
+                    </tr>
+                </thead>
+                <tbody>`;
+
+for (let compra of listadoCompras) {
+    
+    xhtml+= `<tr>
+                <td>${compra.cliente} (${compra.documento})</td>
+                <td>${compra.producto.nombre} - $${compra.producto.precio}</td>
+                <td>${compra.cantidad}</td>
+                <td>${nombresEnvio[compra.envio-1]}</td>
+                <td>$${compra.descuento}</td>
+                <td>$${compra.total}</td>
+            </tr>`;
+
+}
+
+xhtml += `</tbody>
+        </table>`;
+
+
+document.write(xhtml);
+
+/*
+
+let cliente = prompt("Escriba el nombre del cliente...");
+let documento = prompt(`Escriba el documento de ${cliente}...`);
+
+
+
+let menu = "";
+
+for (let i in listaProductos) {
+    let producto = listaProductos[i];
+    menu += `${parseInt(i)+1}. ${producto.nombre} ($${producto.precio})\n`;
 }
 
 let seleccion = parseInt(prompt(`${menu}\nSeleccione el producto...`));
@@ -94,4 +235,4 @@ document.write(`
 <h3>Total valor compra: $${total}</h3>
 `);
 
-
+*/
